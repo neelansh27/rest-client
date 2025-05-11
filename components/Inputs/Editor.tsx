6e328d435c55e867c8ccdcb793000966e0e1d6af
neelansh27 <neelanshsharma30@gmail.com>
2025-05-11 12:38:51 +0530

    Fix editor not loading and add linting

--- components/Inputs/Editor.tsx
+++ components/Inputs/Editor.tsx
@@ -1,28 +1,50 @@
 'use client';
-import {useRef} from "react";
-import {useCodeMirror} from "@uiw/react-codemirror";
-import {json} from "@codemirror/lang-json"
+import {useEffect, useRef} from "react";
+import {Extension, useCodeMirror} from "@uiw/react-codemirror";
+import {json, jsonParseLinter} from "@codemirror/lang-json"
 import {EditorView} from "@uiw/react-codemirror";
+import {linter,lintGutter} from "@codemirror/lint";
 
-interface EditorProps {
+interface EditorOptions {
     text: string;
-    editable?: boolean;
+    className?: string;
+    linting?: boolean;
 }
 
-export default function Editor({text, editable=false }: EditorProps) {
+interface Editable extends EditorOptions{
+    editable: boolean;
+    onChange: (value: string) => void;
+}
+
+interface ReadOnlyEditor extends EditorOptions{
+    editable?: false;
+    onChange?: undefined;
+}
+
+type EditorProps = Editable | ReadOnlyEditor;
+
+export default function Editor({text, className, editable=false, linting=false, onChange }: EditorProps) {
     const codeMirrorRef = useRef<HTMLDivElement | null>(null)
-    useCodeMirror({
+    const extensions:Extension[] = [ json(), EditorView.lineWrapping ];
+    if (linting) {
+        extensions.push(lintGutter(), linter(jsonParseLinter()));
+    }
+    const {setContainer} = useCodeMirror({
         container: codeMirrorRef.current,
         value: text,
-        extensions: [
-            json(),
-            EditorView.lineWrapping,
-        ],
+        extensions: extensions,
         theme: "dark",
         editable: editable,
+        onChange: onChange,
     })
+
+    useEffect(() => {
+       if (codeMirrorRef.current) {
+           setContainer(codeMirrorRef.current)
+       }
+    },[]);
     return (
-        <div ref={codeMirrorRef} className={"h-screen overflow-y-scroll"}>
+        <div ref={codeMirrorRef} className={className || "h-screen overflow-y-scroll"}>
         </div>
     )
 }
