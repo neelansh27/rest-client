9c712931795dbbf650074fa21ced972ee4fa9cc2
neelansh27 <neelanshsharma30@gmail.com>
2025-08-28 15:00:41 +0530

    Update ssl config

--- config/mikro-orm.ts
+++ config/mikro-orm.ts
@@ -1,16 +1,16 @@
 import { RequestHistory } from "@/entities/RequestHistory";
 import { defineConfig } from "@mikro-orm/postgresql";
 import { Migrator } from "@mikro-orm/migrations";
-import {User} from "@/entities/User";
-import {Account} from "@/entities/Account";
-import {Session} from "@/entities/Session";
-import {VerificationToken} from "@/entities/VerificationToken";
+import { User } from "@/entities/User";
+import { Account } from "@/entities/Account";
+import { Session } from "@/entities/Session";
+import { VerificationToken } from "@/entities/VerificationToken";
 console.log([RequestHistory, User, Account, Session, VerificationToken])
 
 const config = defineConfig({
     clientUrl: process.env.DATABASE_URL,
     driverOptions: {
-        connection: { ssl: true }
+        connection: { ssl: { rejectUnauthorized: false } },
     },
     entities: [RequestHistory, User, Account, Session, VerificationToken],
     debug: process.env.DEBUG === "true",
